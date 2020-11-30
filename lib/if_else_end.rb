f02f1cc1502ca13679fd2efd339867b30af0d6dc
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 20:58:44 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,12 @@
-# Write your solution here
+
+current_time = Time.now 
+current_time = current_time.to_i
+
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
+
+# when trying to submit this lab, as written multiple times from the 'solving the lab' examples, kept receiving parse and parse stream errors. Spoke with coach, screenshared, and they were unable to figure out what was going on with the code. They think it could be something written in the spec file or with ruby in this IDE lab. They flagged this lab on my account and recommened that I use REPL Ruby - Repl.it/languages/ruby through my browser. I was able to see how this method would work. 
